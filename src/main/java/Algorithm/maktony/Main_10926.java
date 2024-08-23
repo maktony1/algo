aef159b5e5b3a353119a9e79fc3da2adf89185e4
ghkdehdgnl12 <ghkdehdgnl12@gmail.com>
2024-08-23 05:09:57 +0000

    10926 sc close 누락 추가

--- src/main/java/Algorithm/maktony/Main_10926.java
+++ src/main/java/Algorithm/maktony/Main_10926.java
@@ -20,6 +20,7 @@ public class Main_10926 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String id = sc.nextLine();
+        sc.close();
         System.out.println(id + "??!");
     }
 }
