d714bd5946b818403f905d76e26aba5e59736cb2
ghkdehdgnl12 <ghkdehdgnl12@naver.com>
2024-09-10 02:22:41 +0000

    버퍼 , 시스템out 테스트

---
+++ src/main/java/Algorithm/maktony/Main_2557.java
@@ -0,0 +1,15 @@
+/**
+
+**/
+package Algorithm.maktony;
+
+import java.io.*;
+
+public class Main_2557 { 
+    public static void main(String[] args) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Hello World!");
+        bw.flush();
+        bw.close();
+    }
+}
