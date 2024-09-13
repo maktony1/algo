e9d805340c75c1165e42db8e58d06dc2406d2a86
maktony1 <ghkdehdgnl12@naver.com>
2024-09-13 17:58:58 +0900

    9095

--- src/main/java/Algorithm/maktony/solved/Main_9095.java
+++ src/main/java/Algorithm/maktony/solved/Main_9095.java
@@ -1,10 +1,8 @@
-package Algorithm.maktony.solving;
+package Algorithm.maktony.solved;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main_9095 {
     public static void main(String[] args) throws IOException {
