075fbb79b115205d757f20aa1faeea06c30caa3a
maktony1 <ghkdehdgnl12@naver.com>
2024-09-13 17:57:49 +0900

    9095

--- src/main/java/Algorithm/maktony/solving/Main_1145.java
+++ src/main/java/Algorithm/maktony/solving/Main_1145.java
@@ -4,25 +4,33 @@ import java.util.*;
 import java.io.*;
 
 public class Main_1145 {
-    public static void main(String[] args) throws IOException{
-        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@");
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int min = 100;
+
+        int min = 10000;
         int[] arr = new int[5];
 
         for (int i = 0; i < 5; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);
 
-        for (int i = 0; i < arr.length; i++) {
-            if(min >= arr[i]){
-                
+        for (int i = 0; i < 3; i++) {
+            for (int j = i + 1; j < 5; j++) {
+                for (int k = j + 1; k < 5; k++) {
+                    if ((min > arr[i] || min > arr[j] || min > arr[k]) && ( ((arr[k]%arr[i])==0) && ((arr[k]%arr[j])==0) ) ){
+                        if((arr[i]*arr[k])%arr[j]==0){
+                            min=arr[i]*arr[k];
+                        }
+                    }
+                }
             }
         }
+        System.out.println(min);
         System.out.println(Arrays.toString(arr));
-    }    
+    }
 }
---
+++ src/main/java/Algorithm/maktony/solving/Main_9095.java
@@ -0,0 +1,29 @@
+package Algorithm.maktony.solving;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_9095 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[] dp = new int[12]; // n이 11보다 작음
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        // 4부터 11까지 DP 배열 채우기
+        for (int i = 4; i <= 11; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+
+        // 테스트 케이스 입력 및 출력
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+    }
+}
